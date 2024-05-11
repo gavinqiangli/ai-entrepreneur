0f738c049f4482aa49c05c001d1829e6f7e9b1e2
pipoop <pipoop@gmail.com>
2024-05-11 13:24:12 +0200

    add ppt generator

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from pptx import Presentation
 
 from langchain.agents import initialize_agent, Tool
 from langchain.agents import AgentType
@@ -53,6 +54,88 @@ def generate_gantt_chart(tasks):
     st.pyplot(fig)
 
 
+def generate_slide_ppt(ppt_json):
+    # Load ppt_json data
+    ppt_data = json.loads(ppt_json)
+    print(ppt_data)
+ 
+    # Extract ppt_title, ppt_slogan, and slide_content
+    ppt_title = ppt_data["ppt_title"]
+    ppt_slogan = ppt_data["ppt_slogan"]
+    slide_content = ppt_data["slide_content"]
+
+    #Hello World! example
+    #../_images/hello-world.png
+    prs = Presentation()
+    title_slide_layout = prs.slide_layouts[0]
+    slide = prs.slides.add_slide(title_slide_layout)
+    title = slide.shapes.title
+    subtitle = slide.placeholders[1]
+
+    title.text = ppt_title
+    subtitle.text = ppt_slogan
+
+    #Bullet slide example
+    #../_images/bullet-slide.png
+    # Iterate through each slide and print its content
+    for slide in slide_content:
+        slide_title = slide["slide_title"]
+        main_bullet = slide["main_bullet"]
+        child_bullet = slide.get('child_bullet', None)  # Default value is None if key doesn't exist
+        grandchild_bullet = slide.get('grandchild_bullet', None)  # Default value is None if key doesn't exist
+
+        print("Slide Title:", slide_title)
+        print("Main Bullet:", main_bullet)
+        print("Child Bullet:", child_bullet)
+        print("Grandchild Bullet:", grandchild_bullet)
+        print("\n")
+
+        bullet_slide_layout = prs.slide_layouts[1]
+
+        slide = prs.slides.add_slide(bullet_slide_layout)
+        shapes = slide.shapes
+
+        title_shape = shapes.title
+        body_shape = shapes.placeholders[1]
+
+        title_shape.text = slide_title
+
+        tf = body_shape.text_frame
+        tf.text = main_bullet
+
+        # Check if child_bullet is a list
+        if child_bullet is not None:
+            if isinstance(child_bullet, list):
+                for bullet in child_bullet:
+                    p = tf.add_paragraph()
+                    p.text = bullet
+                    p.level = 1
+            else:
+                p = tf.add_paragraph()
+                p.text = child_bullet
+                p.level = 1
+
+        # Check if grandchild_bullet is a list
+        if grandchild_bullet is not None:
+            if isinstance(grandchild_bullet, list):
+                for bullet in grandchild_bullet:
+                    p = tf.add_paragraph()
+                    p.text = bullet
+                    p.level = 2
+            else:
+                p = tf.add_paragraph()
+                p.text = grandchild_bullet
+                p.level = 2
+
+    temp_filepath = "test.pptx"
+    prs.save(temp_filepath)
+    with open(temp_filepath, "rb") as template_file:
+        template_byte = template_file.read()
+
+    st.download_button(label="Click to download your test PPT",
+                    data=template_byte,
+                    file_name="test.pptx",
+                    mime='application/octet-stream')
 
 # Create langchain agent with the tools above
 tools = [
@@ -60,14 +143,21 @@ tools = [
         name='generate_result_doc',
         func=generate_result_doc,
         description=(
-            'always use this tool to generate result in a document based on input information'
+            'generate result document based on input information'
+        )
+    ),
+    Tool(
+        name='generate_slide_ppt',
+        func=generate_slide_ppt,
+        description=(
+            'generate ppt slides based on input information'
         )
     ),
     Tool(
         name='generate_gantt_chart',
         func=generate_gantt_chart,
         description=(
-            'always use this tool to generate gantt chart based on input information'
+            'generate gantt chart based on input information'
         )
     ),
 ]
@@ -81,8 +171,11 @@ system_message = SystemMessage(
         2. Prepare 10-slide pitch deck for the startup that would make investors invest millions of dollars without any due diligence. The 10-slide pitch deck can include Title slide, Problem slide, Solution slide, Market opportunity slide, Product demo slide, Customer testimonials slide, Team slide, Revenue and growth slide, Competitive landscape slide, Conclusion slide.
         3. Prepare interview at Y Combinator. Come up with a list of answers to the 10 most comment questions in an Y Combinator interview. The answers should be detailed and quantitative. The list of investor questions should include: What is the idea about and what problem does it solve? Who is your target market and how big is the opportunity? What is your business model and how will you make money? Who is on your team and what are their relevant experiences? How are you different from your competitors and why will you succeed? What is your go-to-market strategy and how will you acquire customers? What is your current traction and what are your plans for growth? What are your biggest risks and how will you mitigate them? How much funding are you seeking and how will you use the funds?
         4. Asking for reasonable funding with detailed cost and task breakdown. 
-        Now you should include all answers from all steps above and use provided documentation tool to show the finished result in a document.
-        After the document, you should also show exhibits. You should input the tasks to the provided gantt chart tool to draw estimated time and MVP plan with key milstones. You should formulate in the format of 'task', 'start_date', 'end_date', convert start date and end date in this way: Month 1 start date is converted to 2024-05-01, end date is 2024-05-31; month 2 start date is converted to 2024-06-01, end date is 2024-06-30. Append all tasks into a correctly formatted json list, make sure it is in a json list format and not dictionary. You should always call the generate_gantt_chart function with the formulated task json list input, and try to generate a detailed gantt chart. Make sure to draw and display the gantt chart using functional call. Don't finish before you draw and display the gantt chart in the end using provided gantt chart tool.
+        You should first include all answers from all steps above and use provided generate documentation tool to show the full document result.
+        After shown the document by tool, you should input the slide deck from step 2 to the provided generate_slide_ppt tool to generate ppt slides. You should first define ppt title and ppt slogan in the title slide. For each of the rest 9 slide you should formulate a json blob including following input: slide_title, main_bullet, child_bullet, grandchild_bullet. You should call the generate_slide_ppt tool with input information of ppt_json containing ppt_title and ppt_slogan for the title slide, and slide_content json array containing the rest 9 slides to generate the entire 10 page slide deck. You should always remember to call the generate_slide_ppt tool to generate the slide deck.
+        After the slides, you should also input the tasks frm step 4 to the provided generate_gantt_chart tool to draw estimated time and MVP plan with key milstones. You should formulate in the format of 'task', 'start_date', 'end_date', convert start date and end date in this way: Month 1 start date is converted to 2024-05-01, end date is 2024-05-31; month 2 start date is converted to 2024-06-01, end date is 2024-06-30. Append all tasks into a correctly formatted json list, make sure it is in a json list format and not dictionary. You should always call the generate_gantt_chart function with the formulated task json list input, and try to generate a detailed gantt chart. Make sure to draw and display the gantt chart using the tool.
+        Don't finish before you have generated slide deck and gantt chart using provided tools.
+        Don't finish before you have generated slide deck and gantt chart using provided tools.
         """
 )
 
--- generateppt.py
+++ generateppt.py
@@ -1,13 +1,65 @@
 from pptx import Presentation
-import os
-import tempfile
 import streamlit as st
-#from st_files_connection import FilesConnection
+import json
 
 
-def generate_file():
-    #Hello World! example
-    #../_images/hello-world.png
+def generate_ppt():
+    # Your slide_content JSON data
+    slide_content_json = """
+    [
+    {
+      "slide_title": "Problem",
+      "main_bullet": "Complex and Time-Consuming Process",
+      "child_bullet": "High costs with patent attorneys, Long wait times, High error rates in manual drafting"
+    },
+    {
+      "slide_title": "Solution",
+      "main_bullet": "AI-Driven Patent Drafting",
+      "child_bullet": "Automates drafting, Provides real-time suggestions, Ensures compliance with laws"
+    },
+    {
+      "slide_title": "Market Opportunity",
+      "main_bullet": "Expansive and Growing Market",
+      "child_bullet": "Global IP software market to reach $5B by 2026, Targeting inventors and corporations"
+    },
+    {
+      "slide_title": "Product Demo",
+      "main_bullet": "Live Demonstration",
+      "child_bullet": "Showcase of drafting tool, Real-time assistance, Error correction features"
+    },
+    {
+      "slide_title": "Customer Testimonials",
+      "main_bullet": "Positive Feedback",
+      "child_bullet": "Ease of use, Cost savings, Improved speed"
+    },
+    {
+      "slide_title": "Team",
+      "main_bullet": "Experienced and Diverse",
+      "child_bullet": "AI researchers, Patent attorneys, Software developers"
+    },
+    {
+      "slide_title": "Revenue and Growth",
+      "main_bullet": "Strong Business Model",
+      "child_bullet": "Subscription-based, Tiered pricing, 5% market capture in 3 years"
+    },
+    {
+      "slide_title": "Competitive Landscape",
+      "main_bullet": "Unique Selling Proposition",
+      "child_bullet": "Faster, cheaper, more accurate than competitors, AI integration"
+    },
+    {
+      "slide_title": "Conclusion",
+      "main_bullet": "Investment Opportunity",
+      "child_bullet": "Scale technology, Expand market reach, Democratize patent services"
+    }
+    ]
+    """
+
+    # Load JSON data
+    slide_content = json.loads(slide_content_json)
+
+    # Hello World! example
+    # ../_images/hello-world.png
     prs = Presentation()
     title_slide_layout = prs.slide_layouts[0]
     slide = prs.slides.add_slide(title_slide_layout)
@@ -17,61 +69,82 @@ def generate_file():
     title.text = "Hello, World!"
     subtitle.text = "python-pptx was here!"
 
-    #Bullet slide example
-    #../_images/bullet-slide.png
-    bullet_slide_layout = prs.slide_layouts[1]
+    # Bullet slide example
+    # ../_images/bullet-slide.png
+    # Iterate through each slide and print its content
+    for slide in slide_content:
+        slide_title = slide["slide_title"]
+        main_bullet = slide["main_bullet"]
+        child_bullet = slide.get('child_bullet', None)  # Default value is None if key doesn't exist
+        grandchild_bullet = slide.get('grandchild_bullet', None)  # Default value is None if key doesn't exist
 
-    slide = prs.slides.add_slide(bullet_slide_layout)
-    shapes = slide.shapes
+        print("Slide Title:", slide_title)
+        print("Main Bullet:", main_bullet)
+        print("Child Bullet:", child_bullet)
+        print("Grandchild Bullet:", grandchild_bullet)
+        print("\n")
 
-    title_shape = shapes.title
-    body_shape = shapes.placeholders[1]
+        bullet_slide_layout = prs.slide_layouts[1]
 
-    title_shape.text = 'Adding a Bullet Slide'
+        slide = prs.slides.add_slide(bullet_slide_layout)
+        shapes = slide.shapes
 
-    tf = body_shape.text_frame
-    tf.text = 'Find the bullet slide layout'
+        title_shape = shapes.title
+        body_shape = shapes.placeholders[1]
 
-    p = tf.add_paragraph()
-    p.text = 'Use _TextFrame.text for first bullet'
-    p.level = 1
+        title_shape.text = slide_title
 
-    p = tf.add_paragraph()
-    p.text = 'Use _TextFrame.add_paragraph() for subsequent bullets'
-    p.level = 2
+        tf = body_shape.text_frame
+        tf.text = main_bullet
+
+        # Check if child_bullet is a list
+        if child_bullet is not None:
+            if isinstance(child_bullet, list):
+                for bullet in child_bullet:
+                    p = tf.add_paragraph()
+                    p.text = bullet
+                    p.level = 1
+            else:
+                p = tf.add_paragraph()
+                p.text = child_bullet
+                p.level = 1
+
+        # Check if grandchild_bullet is a list
+        if grandchild_bullet is not None:
+            if isinstance(grandchild_bullet, list):
+                for bullet in grandchild_bullet:
+                    p = tf.add_paragraph()
+                    p.text = bullet
+                    p.level = 2
+            else:
+                p = tf.add_paragraph()
+                p.text = grandchild_bullet
+                p.level = 2
 
     return prs
+
 
 # save locally
 # prs = generate_file()
 # prs.save('test.pptx')
 
-# save on streamlit cloud
+
+# save on streamlit cloud (download file to your web browser)
 @st.cache_resource(ttl="1h")
 def save_file():
-    temp_dir = tempfile.TemporaryDirectory()
-    #temp_filepath = os.path.join(temp_dir.name, 'test.pptx')
     temp_filepath = "test.pptx"
-    prs = generate_file()
+    prs = generate_ppt()
     prs.save(temp_filepath)
     return temp_filepath
-
-    # Create connection object and retrieve file contents.
-    # Specify input format is a csv and to cache the result for 600 seconds.
-    # conn = st.connection('gcs', type=FilesConnection)
-    # df = conn.read("streamlit-bucket/myfile.csv", input_format="csv", ttl=600)
-
-    # client = storage.Client.from_service_account_json(os.environ.get('GCP_CREDENTIALS_PATH'))
-    # bucket = client.bucket(os.environ.get('GCP_BUCKET_NAME'))
-    # blob = bucket.blob("test.pptx")
-    # blob.upload_from_file(source_file)
 
 
 temp_filepath = save_file()
 with open(temp_filepath, "rb") as template_file:
-        template_byte = template_file.read()
+    template_byte = template_file.read()
 
-st.download_button(label="Click to Download Template File",
-                    data=template_byte,
-                    file_name="test.pptx",
-                    mime='application/octet-stream')
+st.download_button(
+    label="Click to download your test PPT",
+    data=template_byte,
+    file_name="test.pptx",
+    mime="application/octet-stream",
+)
